c90b2eb051c3304dcb39d3439bcc8b97cbaac7c7
chenquan <chenquan.dev@gmail.com>
2022-03-23 08:45:53 +0800

    perf: optimize select

--- session/db.go
+++ session/db.go
@@ -4,7 +4,6 @@ import (
 	"context"
 	"database/sql"
 	"errors"
-	"fmt"
 	"reflect"
 	"strings"
 
@@ -156,9 +155,21 @@ func (d *DB[T]) FirstContext(ctx context.Context) (result T, err error) {
 
 	return results[0], nil
 }
+
 func (d *DB[T]) Count() (n int64, err error) {
 	return d.CountContext(context.Background())
 }
+
+func (d *DB[T]) CountContext(ctx context.Context) (n int64, err error) {
+	d.clause.Set(clause.Count, d.RefTable().TableName)
+	sqlStr, vars := d.clause.Build(clause.Count, clause.Where)
+	row := d.Raw(sqlStr, vars...).QueryRowContext(ctx)
+	if err = row.Scan(&n); err != nil {
+		return
+	}
+	return
+}
+
 func (d *DB[T]) Update(record map[string]interface{}) (int64, error) {
 	return d.UpdateContext(context.Background(), record)
 }
@@ -188,16 +199,6 @@ func (d *DB[T]) UpdateContext(ctx context.Context, record map[string]interface{}
 	return result.RowsAffected()
 }
 
-func (d *DB[T]) CountContext(ctx context.Context) (n int64, err error) {
-	d.clause.Set(clause.Count, d.RefTable().TableName)
-	sqlStr, vars := d.clause.Build(clause.Count, clause.Where)
-	row := d.Raw(sqlStr, vars...).QueryRowContext(ctx)
-	if err = row.Scan(&n); err != nil {
-		return
-	}
-	return
-}
-
 func (d *DB[T]) Limit(num int) *DB[T] {
 	d.clause.Set(clause.Limit, num)
 	return d
@@ -212,31 +213,4 @@ func (d *DB[T]) Where(desc string, args ...interface{}) *DB[T] {
 func (d *DB[T]) OrderBy(desc string) *DB[T] {
 	d.clause.Set(clause.OrderBy, desc)
 	return d
-}
-
-func (s *Session[T]) Transaction(txFn func(tx *Tx[T]) error) (err error) {
-	var tx *Tx[T]
-	if tx, err = s.Begin(); err != nil {
-		return
-	}
-
-	defer func() {
-		if p := recover(); p != nil {
-			_ = tx.Rollback()
-			panic(p) // re-throw panic after Rollback
-		} else if err != nil {
-			rollbackErr := tx.Rollback() // err is non-nil; don't change it
-			if rollbackErr != nil {
-				err = fmt.Errorf("excute err: %w, tx error: %s", err, rollbackErr.Error())
-			}
-		} else {
-			err = tx.Commit() // err is nil; if Commit returns error update err
-		}
-	}()
-
-	return txFn(tx)
-}
-
-func (t *Tx[T]) NotTransaction(txFn func(db *DB[T]) error) (err error) {
-	return txFn(t.cloneDB())
 }
--- session/tx.go
+++ session/tx.go
@@ -3,6 +3,7 @@ package session
 import (
 	"context"
 	"database/sql"
+	"fmt"
 )
 
 func (d *DB[T]) cloneDB() *DB[T] {
@@ -16,6 +17,33 @@ func (d *DB[T]) cloneDB() *DB[T] {
 		clause:   d.clause,
 	}
 }
+func (s *Session[T]) Transaction(txFn func(tx *Tx[T]) error) (err error) {
+	var tx *Tx[T]
+	if tx, err = s.Begin(); err != nil {
+		return
+	}
+
+	defer func() {
+		if p := recover(); p != nil {
+			_ = tx.Rollback()
+			panic(p) // re-throw panic after Rollback
+		} else if err != nil {
+			rollbackErr := tx.Rollback() // err is non-nil; don't change it
+			if rollbackErr != nil {
+				err = fmt.Errorf("excute err: %w, tx error: %s", err, rollbackErr.Error())
+			}
+		} else {
+			err = tx.Commit() // err is nil; if Commit returns error update err
+		}
+	}()
+
+	return txFn(tx)
+}
+
+func (t *Tx[T]) NotTransaction(txFn func(db *DB[T]) error) (err error) {
+	return txFn(t.cloneDB())
+}
+
 func (s *Session[T]) Begin() (tx *Tx[T], err error) {
 	return s.BeginTx(context.Background())
 }
