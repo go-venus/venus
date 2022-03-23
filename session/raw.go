f9ce81a74d0a583bc2da4ed37eafda21a7132caf
chen quan <chenquan.dev@gmail.com>
2022-03-23 08:46:22 +0800

    perf: optimize select (#1)
    * perf: optimize select
    
    * perf: optimize select

--- session/raw.go
+++ session/raw.go
@@ -10,53 +10,53 @@ import (
 	"go.uber.org/zap"
 )
 
-func (s *DB[T]) Raw(sql string, values ...any) *DB[T] {
-	s.sql.WriteString(sql)
-	s.sql.WriteString(" ")
-	s.sqlVars = append(s.sqlVars, values...)
-	log.Debug("sql", zap.String("sql", s.sql.String()), zap.Any("param", s.sqlVars))
-	fmt.Println(s.sql.String(), s.sqlVars)
-	return s
+func (d *DB[T]) Raw(sql string, values ...any) *DB[T] {
+	d.sql.WriteString(sql)
+	d.sql.WriteString(" ")
+	d.sqlVars = append(d.sqlVars, values...)
+	log.Debug("sql", zap.String("sql", d.sql.String()), zap.Any("param", d.sqlVars))
+	fmt.Println(d.sql.String(), d.sqlVars)
+	return d
 }
 
-func (s *DB[T]) QueryRow() *sql.Row {
-	return s.QueryRowContext(context.Background())
+func (d *DB[T]) QueryRow() *sql.Row {
+	return d.QueryRowContext(context.Background())
 }
 
-func (s *DB[T]) QueryRowContext(ctx context.Context) *sql.Row {
-	defer s.Clear()
-	return s.getDB().QueryRowContext(ctx, s.sql.String(), s.sqlVars...)
+func (d *DB[T]) QueryRowContext(ctx context.Context) *sql.Row {
+	defer d.Clear()
+	return d.getDB().QueryRowContext(ctx, d.sql.String(), d.sqlVars...)
 }
 
-func (s *DB[T]) QueryRows() (rows *sql.Rows, err error) {
-	return s.QueryRowsContext(context.Background())
+func (d *DB[T]) QueryRows() (rows *sql.Rows, err error) {
+	return d.QueryRowsContext(context.Background())
 }
 
-func (s *DB[T]) QueryRowsContext(ctx context.Context) (rows *sql.Rows, err error) {
-	defer s.Clear()
-	rows, err = s.getDB().QueryContext(ctx, s.sql.String(), s.sqlVars...)
+func (d *DB[T]) QueryRowsContext(ctx context.Context) (rows *sql.Rows, err error) {
+	defer d.Clear()
+	rows, err = d.getDB().QueryContext(ctx, d.sql.String(), d.sqlVars...)
 	return
 }
 
-func (s *DB[T]) Exec() (result sql.Result, err error) {
-	return s.ExecContext(context.Background())
+func (d *DB[T]) Exec() (result sql.Result, err error) {
+	return d.ExecContext(context.Background())
 }
 
-func (s *DB[T]) ExecContext(ctx context.Context) (result sql.Result, err error) {
-	defer s.Clear()
-	result, err = s.getDB().ExecContext(ctx, s.sql.String(), s.sqlVars...)
+func (d *DB[T]) ExecContext(ctx context.Context) (result sql.Result, err error) {
+	defer d.Clear()
+	result, err = d.getDB().ExecContext(ctx, d.sql.String(), d.sqlVars...)
 	return
 }
 
-func (s *DB[T]) Clear() {
-	s.sql.Reset()
-	s.sqlVars = nil
-	s.clause = clause.Clause{}
+func (d *DB[T]) Clear() {
+	d.sql.Reset()
+	d.sqlVars = nil
+	d.clause = clause.Clause{}
 }
 
-func (s *DB[T]) getDB() db {
-	if s.tx != nil {
-		return s.tx
+func (d *DB[T]) getDB() db {
+	if d.tx != nil {
+		return d.tx
 	}
-	return s.db
+	return d.db
 }
--- session/tx.go
+++ session/tx.go
@@ -3,19 +3,47 @@ package session
 import (
 	"context"
 	"database/sql"
+	"fmt"
 )
 
-func (s *DB[T]) cloneDB() *DB[T] {
+func (d *DB[T]) cloneDB() *DB[T] {
 	return &DB[T]{
-		model:    s.model,
-		db:       s.db,
-		sql:      s.sql,
-		sqlVars:  s.sqlVars,
-		dialect:  s.dialect,
-		refTable: s.refTable,
-		clause:   s.clause,
+		model:    d.model,
+		db:       d.db,
+		sql:      d.sql,
+		sqlVars:  d.sqlVars,
+		dialect:  d.dialect,
+		refTable: d.refTable,
+		clause:   d.clause,
 	}
 }
+func (s *Session[T]) Transaction(txFn func(tx *Tx[T]) error) (err error) {
+	var tx *Tx[T]
+	if tx, err = s.Begin(); err != nil {
+		return
+	}
+
+	defer func() {
+		if p := recover(); p != nil {
+			_ = tx.Rollback()
+			panic(p) // re-throw panic after Rollback
+		} else if err != nil {
+			rollbackErr := tx.Rollback() // err is non-nil; don't change it
+			if rollbackErr != nil {
+				err = fmt.Errorf("excute err: %w, tx error: %s", err, rollbackErr.Error())
+			}
+		} else {
+			err = tx.Commit() // err is nil; if Commit returns error update err
+		}
+	}()
+
+	return txFn(tx)
+}
+
+func (t *Tx[T]) NotTransaction(txFn func(db *DB[T]) error) (err error) {
+	return txFn(t.cloneDB())
+}
+
 func (s *Session[T]) Begin() (tx *Tx[T], err error) {
 	return s.BeginTx(context.Background())
 }
@@ -33,12 +61,12 @@ func (s *Session[T]) BeginTx(ctx context.Context) (tx *Tx[T], err error) {
 	}, err
 }
 
-func (s *Tx[T]) Commit() (err error) {
-	err = s.tx.Commit()
+func (t *Tx[T]) Commit() (err error) {
+	err = t.tx.Commit()
 	return
 }
 
-func (s *Tx[T]) Rollback() (err error) {
-	err = s.tx.Rollback()
+func (t *Tx[T]) Rollback() (err error) {
+	err = t.tx.Rollback()
 	return
 }
